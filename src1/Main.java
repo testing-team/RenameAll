465bfa28bd8744c19c4231fe5f4bedb99c543b00
malyu <2570637909@qq.com>
2019-06-28 10:28:38 +0800

    代码上传

--- src1/Main.java
+++ src1/Main.java
@@ -1,3 +1,4 @@
+﻿//主方法
 public class Main {
 
     public static void main(String[] args) {
--- src1/Show.java
+++ src1/Show.java
@@ -1,4 +1,4 @@
-
+﻿
 import java.io.File;
 import javax.swing.*;
 import java.awt.*;
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
-
+//主界面窗口
 @SuppressWarnings("serial")
 public class Show extends JFrame implements ActionListener, FocusListener {
         private JMenuBar jmr;// 菜单栏
